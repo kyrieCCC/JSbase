312df22de32b7e4588f61cb49184e528076c5acc
kyrieCCC <1186624948@qq.com>
2022-11-27 22:34:18 +0800

    feat: closure

--- closure.js
+++ closure.js
@@ -6,7 +6,7 @@ for (i = 0; i < 5; i++){
     }
     fn.push(j)
 }
-fn[0]()
+// fn[0]()
 Object.prototype.myCall = function (...arg) {
     const obj = arg.slice(0, 1)
     const arr = arg.slice(1)
@@ -26,4 +26,17 @@ for (i = 0; i < 5; i++){
     })(i)
     fn1.push(j)
 }
-fn1[1]()
+// fn1[1]()
+
+
+const out = 'irving'
+function print() {
+    console.log(out)
+}
+print()
+
+function foo(print) {
+    console.log('我们要引用print函数形成闭包')
+    print()
+}
+foo(print)
